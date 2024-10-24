0ce5bfdbb727e05faec6f0004eb4c4fbc59db6c6
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2024-10-24 17:21:59 +0700

    adj: remove console log in prod

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   // experimental: {
   //   taint: true,
   // },
+  compiler: {
+    // Remove console logs only in production, excluding error logs
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false
+  },
   reactStrictMode: false,
   experimental: {
     serverActions: {
